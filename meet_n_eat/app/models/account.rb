7a568afb75c72814ce1ca2cc4aad9ce4de69b786
github username <github email address>
2017-03-10 13:21:35 -0500

    finalized relationships(i think)

--- meet_n_eat/app/models/account.rb
+++ meet_n_eat/app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ApplicationRecord
-	has_many :created_events, class_name: "Event", foreign_key: "creator_id"
-	has_many :account_events, foreign_key: :account_id
+	has_many :created_events, class_name: "Event", foreign_key: "host_id"
+	has_many :account_events, foreign_key: "account_id"
 	has_many :attended_events, through: :account_events
 end
