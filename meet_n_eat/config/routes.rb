7b46b9695ace263b7ef132f468957c755cf2a815
Alexandra Roth <alroth8@gmail.com>
2017-03-10 11:23:11 -0500

    static home

--- meet_n_eat/config/routes.rb
+++ meet_n_eat/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+	root 'static#home'
+
+	get '/signin', to: 'sessions#new'
+	post '/signin', to: 'sessions#create' #change destination
+	delete '/signin', to: 'sessions#destroy'
+
+	resources :accounts
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
