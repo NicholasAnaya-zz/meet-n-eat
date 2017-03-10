0c2530ee0133eb14d9d344165c3bd8538872cee2
Alexandra Roth <alroth8@gmail.com>
2017-03-10 13:23:48 -0500

    starting sessions

--- meet_n_eat/app/controllers/sessions_controller.rb
+++ meet_n_eat/app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
 
 	def new
+		@account = Account.new
 	end
 
 	def create
+		if params[:username] && !params[:username].empty?
+			session[:username] = param[:username]
+			redirect_to account_path(@account)
+		else
+			flash[:error] = "Please Enter a Valid Username and Password"
+			redirect_to root_path
+		end
 	end
 
 	def destroy
+		session.delete(:username)
 	end
-	
+
 end
--- meet_n_eat/config/routes.rb
+++ meet_n_eat/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 	get '/signin', to: 'sessions#new'
 	post '/signin', to: 'sessions#create' #change destination
-	delete '/signin', to: 'sessions#destroy'
+	delete '/signout', to: 'sessions#destroy'
 
 
 
