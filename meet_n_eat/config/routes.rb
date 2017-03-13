23d00f8f4d939f05ce0b5cf46616803e7af0a757
github username <github email address>
2017-03-13 11:28:44 -0400

    added nested params for accounts/events

--- meet_n_eat/config/routes.rb
+++ meet_n_eat/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :events
-  resources :accounts
+  resources :accounts do
+  	resources :events, only: [:index, :show]
+  end
 
 end
