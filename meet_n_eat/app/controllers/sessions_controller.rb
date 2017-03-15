150a4a35bd750cbad8686802837c2966bb6980f2
github username <github email address>
2017-03-14 21:22:36 -0400

    furnished the site's current features

--- meet_n_eat/app/controllers/sessions_controller.rb
+++ meet_n_eat/app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
 		account = Account.find_by(username: params[:account][:username])
 		if (account && account.authenticate(params[:account][:password])) 
 			session[:account_id] = account.id
-			redirect_to account_path(account)
+			flash[:message] = "Successfully logged in as #{current_account.username}."
+			redirect_to root_path
 		else
 			flash[:error] = "Your Username or Password is not valid. If you do not have an account, please signup to find dinner buddies!"
 			redirect_to signin_path
